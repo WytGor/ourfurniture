cdcacebe975ba1da70800b3e260be8daccdca835
iliya <altabaev2013@gmail.com>
2019-03-30 18:14:12 +0400

    Добавление ссыло на соц сети

--- wp-content/themes/customfurniture/footer.php
+++ wp-content/themes/customfurniture/footer.php
@@ -6,17 +6,15 @@ wp_footer();
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<?php if (have_rows('social')) : ?>
 					<div class="socials">
-<!--						<a href="#" class="socialLink">-->
-<!--							<img src="--><?//= get_template_directory_uri() . "/images/vk.svg" ?><!--" alt="" class="socialLink__icon">-->
-<!--						</a>-->
-<!--						<a href="#" class="socialLink">-->
-<!--							<img src="--><?//= get_template_directory_uri() . "/images/instagram.svg" ?><!--" alt="" class="socialLink__icon">-->
-<!--						</a>-->
-<!--						<a href="#" class="socialLink">-->
-<!--							<img src="--><?//= get_template_directory_uri() . "/images/whatsapp.svg" ?><!--" alt="" class="socialLink__icon">-->
-<!--						</a>-->
+						<?php while (have_rows('social')) : the_row() ?>
+							<a href="<?php the_sub_field('link') ?>" class="socialLink">
+								<img src="<?php the_sub_field('icon') ?>" alt="" class="socialLink__icon">
+							</a>
+						<?php endwhile; ?>
 					</div>
+					<?php endif; ?>
 					<p class="footer__copyright">
 						&#169; Мебель в Ростове 2018
 					</p>
